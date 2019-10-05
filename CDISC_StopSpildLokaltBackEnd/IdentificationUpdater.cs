c1d9e4011342e829e50a4c4951fd8714ad370b10
Lauritz Baess-Lehmann <lauritzbaess-lehmann@Lauritzs-MacBook-Pro-3.local>
2019-10-05 16:54:32 +0200

    Additional work

---
+++ CDISC_StopSpildLokaltBackEnd/IdentificationUpdater.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace CDISC_StopSpildLokaltBackEnd {
+    public class IdentificationUpdater : IHostedService {
+
+        private static string cron = "0 0 3 1/1 * ? *";
+        // https://thinkrethink.net/2018/05/31/run-scheduled-background-tasks-in-asp-net-core/
+
+        public IdentificationUpdater() {
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken) {
+            throw new NotImplementedException();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken) {
+            throw new NotImplementedException();
+        }
+    }
+}
